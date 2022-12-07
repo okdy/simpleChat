806162d95535525e6ff9b0ffb5f3a3b0a3dcf7a3
okdy <roopie123@daum.net>
2022-12-07 15:16:44 +0900

    default structure

---
+++ src/client/Main.java
@@ -0,0 +1,19 @@
+package client;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main() {
+        setSize(500, 500);
+        setLocation(100, 100);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        new Main();
+
+    }
+
+}
