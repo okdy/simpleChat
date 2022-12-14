f203589d6bdd2f9919c3d0815657f23377fe5a20
okdy <roopie123@daum.net>
2022-12-14 10:15:34 +0900

    serializable object modify

--- src/ChatMember.java
+++ src/ChatMember.java
@@ -4,4 +4,14 @@ public class ChatMember implements Serializable {
 
     private static final Long serialVersionUID = 123L;
 
+    private String name;
+
+    public ChatMember(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chat{name='%s'}", name);
+    }
 }
